8db85419323d799f1c5d78069a628738ba1e38e9
George Mason <georgemason321@hotmail.com>
2017-07-31 22:04:58 +0100

    Added ui elements for search

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,18 @@
 class VehiclesController < ApplicationController
+
+  def new
+  	@carSearch = { make: "", model: "", minprice: "", maxprice: "" }
+  end
+
+  def search
+  	
+  	render 'browser'
+  end
+
   def browser
   end
 
   def profile
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   get 'vehicles/profile'
 
   root 'static_pages#home'
-  get '/help',      to: 'static_pages#help'
-  get '/about',     to: 'static_pages#about'
-  get '/contact',   to: 'static_pages#contact'
-  get '/signup',    to: 'users#new'
-  post '/signup',   to: 'users#create'
-  get '/login',     to: 'sessions#new'
-  post '/login',    to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/help',       to: 'static_pages#help'
+  get '/about',      to: 'static_pages#about'
+  get '/contact',    to: 'static_pages#contact'
+  get '/signup',     to: 'users#new'
+  post '/signup',    to: 'users#create'
+  get '/login',      to: 'sessions#new'
+  post '/login',     to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/carsearch',  to: 'vehicles#new'
+  post '/carsearch', to: 'vehicles#search'
   resources :users
 end
