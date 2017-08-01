87aa84d4b49c32f5b7e066545794f4a38567df83
George Mason <georgemason321@hotmail.com>
2017-08-01 22:28:02 +0100

    Finish car browser page simple

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,2 +1,9 @@
 module VehiclesHelper
+
+	def small_photo(car)
+	  if !car.photos.where(scale: "small").blank?
+	  	return car.photos.where(scale: "small").first.path
+	  end
+	  return "/images/nopic.png"
+	end
 end
